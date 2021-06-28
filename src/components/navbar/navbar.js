84762e3dc437338b26a4854599721cd761bd8bb7
Hrishikesh-Bhagwat <bhagwatcoder@gmail.com>
2021-06-29 04:14:32 +0530

    Created Navbar Component

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,21 @@
+import "bulma/css/bulma.css";
+import "./navbar_style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+  return (
+    <div className="">
+      <nav
+      className="navbar is-dark"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <div className="brand-font">Open Authenticator</div>
+      </div>
+    </nav>
+    </div>
+  );
+}
+
+export default Navbar;
