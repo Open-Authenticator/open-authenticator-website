6c304ad12a12f0302794ace221f64a4708efcb8d
Hrishikesh-Bhagwat <bhagwatcoder@gmail.com>
2021-07-01 22:30:28 +0530

    URL typo

--- src/functions/urls.js
+++ src/functions/urls.js
@@ -1,3 +1,3 @@
-const IP="http://192.168.4.1"
+const IP="http://192.168.4.1/"
 const imageURL="https://raw.githubusercontent.com/Open-Authenticator/open-authenticator-app/main/assets/open-autheticator-logo.png"
 export {IP,imageURL}
