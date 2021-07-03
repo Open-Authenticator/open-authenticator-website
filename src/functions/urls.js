15e4a01f73b2ad18b0fde8befe6be3365f50f97d
Hrishikesh-Bhagwat <bhagwatcoder@gmail.com>
2021-07-04 01:32:00 +0530

    Changed IP

--- src/functions/urls.js
+++ src/functions/urls.js
@@ -1,4 +1,4 @@
-const IP="http://192.168.4.1"
+const IP="http://192.168.4.1/"
 //const imageURL="https://raw.githubusercontent.com/Open-Authenticator/open-authenticator-app/main/assets/open-autheticator-logo.png"
 const imageURL="logo.png"
 export {IP,imageURL}
